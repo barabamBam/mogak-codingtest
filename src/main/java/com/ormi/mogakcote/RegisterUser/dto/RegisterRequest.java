b2ac16dacbdd31325e4d98a235a5bd84ff563f05
BaekEuiHun <euihunbaek@gmail.com>
2024-08-29 15:31:40 +0900

    registerUser/a - 회원가입 요청

---
+++ src/main/java/com/ormi/mogakcote/RegisterUser/dto/RegisterRequest.java
@@ -0,0 +1,52 @@
+package com.ormi.mogakcote.RegisterUser.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
+    @JsonProperty("confirmPassword")
+    private String confirmPassword;
+    private String name;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
