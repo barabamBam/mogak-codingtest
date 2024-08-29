59cc36e2783ec329a6f638a8478c3a2da2de1743
BaekEuiHun <euihunbaek@gmail.com>
2024-08-29 15:28:51 +0900

    패스워드 유효 요청에 대한 response

---
+++ src/main/java/com/ormi/mogakcote/RegisterUser/dto/ValidatePasswordResponse.java
@@ -0,0 +1,30 @@
+package com.ormi.mogakcote.RegisterUser.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ValidatePasswordResponse {
+    @JsonProperty("isValid")
+    private boolean isValid;
+    private String[] errors;
+
+    public ValidatePasswordResponse(boolean isValid, String error) {
+        this.isValid = isValid;
+        this.errors = error != null ? new String[]{error} : new String[]{};
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
+    }
+
+    public String[] getErrors() {
+        return errors;
+    }
+
+    public void setErrors(String[] errors) {
+        this.errors = errors;
+    }
+}
