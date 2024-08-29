3b217260e418cbd4687abd83a0092f32873d7dd6
LeeJaejun98 <jj980302@naver.com>
2024-08-29 13:27:48 +0900

    Feat: 공지사항 레포 생성

---
+++ src/main/java/com/ormi/mogakcote/notice/infrastructure/NoticeRepository.java
@@ -0,0 +1,16 @@
+package com.ormi.mogakcote.notice.infrastructure;
+
+import com.ormi.mogakcote.notice.domain.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    Optional<Notice> findByNoticeId(Long noticeId);
+    List<Notice> findAllByOrderByCreatedAtDesc();
+}
