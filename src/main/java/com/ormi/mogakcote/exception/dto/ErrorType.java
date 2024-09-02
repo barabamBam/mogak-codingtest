be91def457f2e71b9e19540eaa70ec947cdc26a1
LeeJaejun98 <jj980302@naver.com>
2024-09-02 14:30:40 +0900

    Fix : 예외처리 오류 수정

--- src/main/java/com/ormi/mogakcote/exception/dto/ErrorType.java
+++ src/main/java/com/ormi/mogakcote/exception/dto/ErrorType.java
@@ -34,7 +34,6 @@ public enum ErrorType {
     ONLY_ADMIN_AUTHORITY_ERROR(HttpStatus.BAD_REQUEST, "관리자 권한이 필요한 기능입니다."),
 
     // notice 예외
-    NOTICE_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "공지사항을 찾을 수 없습니다."),
     RESET_PASSWORD_CODE_NOT_MATCH_ERROR(HttpStatus.BAD_REQUEST, "초기화 코드가 일치하지 않습니다"),
 
     // user 예외
