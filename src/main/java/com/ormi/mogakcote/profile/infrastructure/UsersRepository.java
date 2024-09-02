5547c1cbf0a8c749e28266b3d694fdad193c1d3b
BaekEuiHun <euihunbaek@gmail.com>
2024-09-02 14:47:43 +0900

    해당 패키지 삭제

--- src/main/java/com/ormi/mogakcote/profile/infrastructure/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ormi.mogakcote.profile.infrastructure;
-
-import com.ormi.mogakcote.profile.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-    Users findByNickname(String nickname);
-}
