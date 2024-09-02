b1005d91200179b3f644bb6fc5d3d94ba13f3277
LeeJaejun98 <jj980302@naver.com>
2024-09-02 14:19:42 +0900

    Comment : post객체 없어서 주석 처리함

--- src/main/java/com/ormi/mogakcote/comment/presentation/SystemCommentController.java
+++ src/main/java/com/ormi/mogakcote/comment/presentation/SystemCommentController.java
@@ -1,25 +1,25 @@
-package com.ormi.mogakcote.comment.presentation;
-
-import com.ormi.mogakcote.comment.application.SystemCommentService;
-import com.ormi.mogakcote.common.model.ResponseDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path = "/api/v1/posts/{postId}/system-comments")
-@RequiredArgsConstructor
-public class SystemCommentController {
-    private final SystemCommentService systemCommentService;
-
-    @GetMapping
-    public ResponseEntity<?> getSystemComment(
-            @PathVariable("postId") Long postId
-    ) {
-        var responses = systemCommentService.getSystemComment(postId);
-        return ResponseDto.ok(responses);
-    }
-}
+//package com.ormi.mogakcote.comment.presentation;
+//
+//import com.ormi.mogakcote.comment.application.SystemCommentService;
+//import com.ormi.mogakcote.common.model.ResponseDto;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping(path = "/api/v1/posts/{postId}/system-comments")
+//@RequiredArgsConstructor
+//public class SystemCommentController {
+//    private final SystemCommentService systemCommentService;
+//
+//    @GetMapping
+//    public ResponseEntity<?> getSystemComment(
+//            @PathVariable("postId") Long postId
+//    ) {
+//        var responses = systemCommentService.getSystemComment(postId);
+//        return ResponseDto.ok(responses);
+//    }
+//}
