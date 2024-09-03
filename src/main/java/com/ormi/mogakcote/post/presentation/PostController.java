4e24c2499e98185db529909ed7103f0c5e8afbc1
LeeJaejun98 <jj980302@naver.com>
2024-09-03 20:34:53 +0900

    Feat : convertBanned 추가 Fix : api 수정

--- src/main/java/com/ormi/mogakcote/post/presentation/PostController.java
+++ src/main/java/com/ormi/mogakcote/post/presentation/PostController.java
@@ -6,6 +6,7 @@ import com.ormi.mogakcote.post.dto.request.PostRequest;
 import com.ormi.mogakcote.post.application.PostService;
 import com.ormi.mogakcote.common.dto.SuccessResponse;
 import com.ormi.mogakcote.post.dto.response.PostResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,43 +14,53 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/post")
+@RequestMapping()
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
-    @PostMapping
+    @PostMapping("/api/v1/post")
     public ResponseEntity<?> writePost(
-        AuthUser user,
-        @RequestBody PostRequest request
+            AuthUser user,
+            @RequestBody PostRequest request
     ) {
         var response = postService.createPost(user, request);
         return ResponseDto.created(response);
     }
 
 
-    @GetMapping("/{postId}")
+    @GetMapping("/api/v1/post/{postId}")
     public ResponseEntity<PostResponse> getPost(@PathVariable(name = "postId") Long postId) {
         PostResponse post = postService.getPost(postId);
         return ResponseEntity.ok(post);
     }
 
-    @GetMapping
+    @GetMapping("/api/v1/post")
     public ResponseEntity<List<PostResponse>> getAllPosts() {
         List<PostResponse> posts = postService.getAllPosts();
         return ResponseEntity.ok(posts);
     }
 
-    @PutMapping("/{postId}")
+    @PutMapping("/api/v1/post/{postId}")
     public ResponseEntity<?> modifyPost(AuthUser user, @PathVariable(name = "postId") Long postId, @RequestBody PostRequest postRequest) {
         PostResponse updatedPost = postService.updatePost(user, postId, postRequest);
         return ResponseEntity.ok(updatedPost);
     }
 
-    @DeleteMapping("/{postId}")
+    @DeleteMapping("/api/v1/post/{postId}")
     public ResponseEntity<SuccessResponse> deletePost(AuthUser user, @PathVariable(name = "postId") Long postId) {
-        postService.deletePost(user,postId);
+        postService.deletePost(user, postId);
         return ResponseEntity.ok(new SuccessResponse("게시글 삭제 성공"));
+    }
+
+    @PutMapping("/api/v1/admin/convertBanned/{postId}")
+    public ResponseEntity<?> convertBanned(
+            AuthUser user,
+            @PathVariable(name = "postId") Long id,
+            @RequestBody @Valid PostRequest request
+    ) {
+        PostResponse updateBanned = postService.convertBanned(id, request);
+        return ResponseEntity.ok(updateBanned);
     }
 }
