7f0aed0fbe03bff2c757118923b1ee54dcaec6ae
Minju Kim <72141158+barabamBam@users.noreply.github.com>
2024-09-04 15:50:37 +0900

    feat: 게시글 목록 페이지 기능 -> 코드 일부 수정

--- src/main/java/com/ormi/mogakcote/problem/infrastructure/ProblemReportAlgorithmRepository.java
+++ src/main/java/com/ormi/mogakcote/problem/infrastructure/ProblemReportAlgorithmRepository.java
@@ -2,6 +2,7 @@ package com.ormi.mogakcote.problem.infrastructure;
 
 import com.ormi.mogakcote.problem.domain.ProblemReportAlgorithm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +12,7 @@ public interface ProblemReportAlgorithmRepository extends
         JpaRepository<ProblemReportAlgorithm, Long> {
 
     boolean existsByAlgorithmIdAndProblemReportId(Long algorithmId, Long problemReportId);
+
+	@Query("select p.algorithmId from ProblemReportAlgorithm p where p.problemReportId = ?1")
+	Long findAlgorithmIdByProblemReportId(Long probReportId);
 }
